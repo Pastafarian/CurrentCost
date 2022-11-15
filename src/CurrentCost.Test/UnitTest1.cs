9466c91d7b59c76665ea2cec309453eb8fbabde5
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2022-11-15 23:02:07 +0000

    Removed docker as it will not play nicely with serial port USB emulator

--- src/CurrentCost.Test/UnitTest1.cs
+++ src/CurrentCost.Test/UnitTest1.cs
@@ -1,11 +1,10 @@
-namespace CurrentCost.Test
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+namespace CurrentCost.Test;
 
-        }
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
     }
 }
