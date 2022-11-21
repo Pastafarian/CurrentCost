c24d3fb663ce312e24df36945a8177920ccb3c93
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2022-11-21 18:58:37 +0000

    Setup bootstrap, font-awesome and chart js

--- src/CurrentCost/Program.cs
+++ src/CurrentCost/Program.cs
@@ -1,9 +1,21 @@
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
+
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+
+
+
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddBlazorise(options => options.Immediate = true);
+builder.Services.AddBootstrap5Providers();
+builder.Services.AddFontAwesomeIcons();
 
 var app = builder.Build();
 
